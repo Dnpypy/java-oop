ef890793e7692dc912887dcfd8b04a4916d6f152
Dnpypy <denlatypoff@yandex.ru>
2022-01-24 17:51:28 +0300

    Solution JavaStackHeap

--- src/section001_classes/Solution.java
+++ src/section001_classes/Solution.java
@@ -4,6 +4,6 @@ public class Solution {
     public static void main(String[] args) {
         int[] array = {1, -2, -3, 4, -5};
 
-        //...getPositiveNumbers
+        //DynaArray...getPositiveNumbers
     }
 }
