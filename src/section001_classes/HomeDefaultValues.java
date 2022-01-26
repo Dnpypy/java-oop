774b15f54651784c644624e7e7963955fb664fd9
Dnpypy <denlatypoff@yandex.ru>
2022-01-26 13:54:34 +0300

    DefaultValues

---
+++ src/section001_classes/HomeDefaultValues.java
@@ -0,0 +1,32 @@
+package section001_classes;
+
+/**
+ * boolean aBoolean = false;
+ * byte aByte = 0;
+ * short aShort = 0;
+ * int anInt = 0;
+ * long aLong = 0;
+ * float aFloat = 0.0;
+ * double aDouble = 0.0;
+ * char aChar = 0;
+ * String string = null;
+ * int[] array = null;
+ * DynaArray dynaArray = null;
+ */
+public class HomeDefaultValues {
+    boolean aBoolean;
+    byte aByte;
+    short aShort;
+    int anInt;
+    long aLong;
+    float aFloat;
+    double aDouble;
+    char aChar;
+    String string;
+    int[] array;
+    DynaArray dynaArray;
+
+    public static void main(String[] args) {
+        //вывезти значения по умолчанию полей класса DefaultValues
+    }
+}
--- src/section001_classes/Solution.java
+++ src/section001_classes/Solution.java
@@ -4,27 +4,26 @@ public class Solution {
     public static void main(String[] args) {
         int[] array = {1, -2, -3, 4, -5};
 
-        DynaArray result = getPositiveNumbers(array);
+        DynaArray dynaArray = getPositiveNumbers(array);
 
-        for (int i = 0; i < result.count; i++) {
-            System.out.print(result.result[i] + " ");
+        for (int i = 0; i < dynaArray.count; i++) {
+            System.out.print(dynaArray.result[i] + " ");
         }
 
     }
 
     private static DynaArray getPositiveNumbers(int[] array) {
-        int[] result = new int[array.length];
-        int count = 0;
-
-        for (int j : array) {
-            if (j > 0) {
-                result[count] = j;
-                count++;
+        int[] newArr = new int[array.length];
+        var n1 = 0;
+        for (Integer i : array) {
+            if(i > 0) {
+                newArr[n1] = i;
+                n1++;
             }
         }
         DynaArray dynaArray = new DynaArray();
-        dynaArray.result = result;
-        dynaArray.count = count;
+        dynaArray.result = newArr;
+        dynaArray.count = n1;
         return dynaArray;
     }
 }
