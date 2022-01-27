0c70e8660acf2bb5e4881a806e6b8109c7b1f7cf
Dnpypy <denlatypoff@yandex.ru>
2022-01-27 11:58:51 +0300

    DynaArrayTest DynaArrayVer3 SolutionVer3

---
+++ src/section001_classes/DynaArrayTest.java
@@ -0,0 +1,10 @@
+package section001_classes;
+
+public class DynaArrayTest {
+    public static void main(String[] args) {
+        DynaArrayVer3 dynaArray = new DynaArrayVer3();
+        //DynaArray.add(dynaArray, 23);
+
+        dynaArray.add(23);
+    }
+}
---
+++ src/section001_classes/DynaArrayVer3.java
@@ -0,0 +1,16 @@
+package section001_classes;
+
+public class DynaArrayVer3 {
+    int[] result = new int[5];
+
+    int count;
+
+    void add(int value) {
+        if (count == result.length) {
+            int[] newArray = new int[result.length * 2];
+            System.arraycopy(result, 0, newArray, 0, result.length);
+            result = newArray;
+        }
+        result[count++] = value;
+    }
+}
