7127ccd22c6d9f8c5b5109ceac6518eaaf634124
Dnpypy <denlatypoff@yandex.ru>
2022-02-23 17:29:55 +0300

    Class example ThisUsage

--- src/section001_classes/ClassTemplate.java
+++ src/section001_classes/ClassTemplate.java
@@ -1,16 +1,37 @@
 package section001_classes;
 
+// ${modifier} class ${name}
+// where ${modifier} is from set: [default/package, public]
 public class ClassTemplate {
+
     // --------- Fields: ---------
-    // ${type} ${name};
-    int fieldName;
+    // ${modifier} ${type} ${name};
+    // where ${modifier} is from set: [private, default/package, protected, public]
+    private int fieldName;
 
     // ...
 
+    // --------- Constructors: ---------
+
+    // ${modifier} ${simple-class-name} (${param-list}),
+    // where param-list item is ${param-type} ${param-name}, separated by comma and
+    // ${modifier} is from set: [private, default/package, protected, public]
+    public ClassTemplate(int param1Name, boolean param2Name, String param3Name /* ... */) {
+        // constructor body
+    }
+
+    // ...
+
+    // --------- Init sections: ---------
+    {
+        // init section body
+    }
 
     // --------- Methods: ---------
-    // ${return-type} ${name} (${param-list}), where param-list item is ${param-type} ${param-name}, separated by comma
-    void methodName(int param1Name, boolean param2Name, String param3Name /* ... */) {
+    // ${modifier} ${return-type} ${name} (${param-list}),
+    // where param-list item is ${param-type} ${param-name}, separated by comma and
+    // ${modifier} is from set: [private, default/package, protected, public]
+    public void methodName(int param1Name, boolean param2Name, String param3Name /* ... */) {
         // method body
     }
 
