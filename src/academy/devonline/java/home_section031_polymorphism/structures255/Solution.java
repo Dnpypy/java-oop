40fd59b85a282d06c8a54050474317ee41ff7492
Dnpypy <denlatypoff@yandex.ru>
2022-03-16 12:02:58 +0300

    commit create package structure255 and classes

---
+++ src/academy/devonline/java/home_section031_polymorphism/structures255/Solution.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2022. http://devonline.academy
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package academy.devonline.java.home_section031_polymorphism.structures255;
+
+/**
+ * Создать абстрактный класс DataStructure у которого все методы абстрактны
+ * Копировать все методы из BaseDataStructure в класс DataStructure
+ * B вместо тело метода, объявляем все методы делаем абстрактными в классе DataStructure
+ * Добавить оставшиеся методы, которые есть в LinkedList и DynaArray, которых нет в классе DataStructure
+ * BaseDataStructure наследуется от класса DataStructure
+ * Переопределить все методы которые переопределяются в классах наследниках
+ */
+
+public class Solution {
+}
