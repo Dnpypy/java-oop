fe3e768c96bca560f0661db4195f246409339bff
Dnpypy <denlatypoff@yandex.ru>
2022-03-21 17:59:42 +0300

    Update

---
+++ src/academy/devonline/java/home_section031_polymorphism/DataStructure255/Solution.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2022. http://devonline.academy
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package academy.devonline.java.home_section031_polymorphism.DataStructure255;
+
+/**
+ * @author devonline
+ * @link http://devonline.academy/java
+ *
+ * 255 решение в уроке
+ * Перенести 4 класса из урока 255 DynaArray, LinkedList, DynaArrayTest, LinkedListTest
+ * Создать абстрактный класс DataStructure у которого все методы абстрактны
+ * Копируем все методы из BaseDataStructure в DataStructure
+ *
+ * И в классе DataStructure все методы объявляем абстрактными
+ *
+ * Добавить оставшиеся методы из DynaArray и LinkedList в класс DataStructure,
+ * которых нет в базовой структуре данных
+ * BaseDataStructure наследуется от DataStructure
+ *
+ * В классе BaseDataStructure переопределить методы которые переопределятся в этой классе
+ * Проверить в классе BaseDataStructure сигнатуру методов на совпадение с методами в классе
+ * DataStructure и удалить излишнее описание
+ *
+ * Остатки методов, которые совпадат переопределить в классах DynaArray, LinkedList
+ * Запустить тесты.
+ *
+ */
+public class Solution {
+}
