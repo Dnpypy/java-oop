707452a946445105666a4e8b22e86206f03e2cb8
Dnpypy <denlatypoff@yandex.ru>
2022-03-24 15:41:52 +0300

    Create package home_section051_static and class Solution

---
+++ src/academy/devonline/java/home_section051_static/StaticFields654/Solution.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2022. http://devonline.academy
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package academy.devonline.java.home_section051_static.StaticFields654;
+
+/**
+ * @author devonline
+ * @link http://devonline.academy/java
+ *
+ * СОздать класс StaticFields в нем создать точку входа.
+ * СОздать пустое числовое поле value
+ * ОБъявить 2 ссылки типа StaticFields на объекты StaticFields
+ * Присвоить любые числовые значение этим объектам, вывезти их
+ *
+ * Присвоить полю value ключевое слово static
+ * Вывод объектов.
+ *
+ * Статическое поле с объектом в памяти HEAP хранится в единственном экземпляре!
+ *
+ */
+public class Solution {
+}
